a491efe05868ef05ade410e6f58982d3f46c13bb
valeennmendez <mendezvalentin433@gmail.com>
2026-02-15 15:28:38 -0300

    Creacion grafica del login

---
+++ frontend/src/components/login/UsernameInput.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function UsernameInput() {
+	return (
+		<>
+			<label className="input validator w-full h-12 rounded-2xl shadow-none bg-[#08111d]">
+				<svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+					<g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor">
+						<path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+						<circle cx="12" cy="7" r="4"></circle>
+					</g>
+				</svg>
+				<input
+					type="text"
+					className=""
+					required
+					placeholder="Usuario Institucional"
+					pattern="[A-Za-z][A-Za-z0-9\-]*"
+					minLength="3"
+					maxLength="30"
+					title="Only letters, numbers or dash"
+				/>
+			</label>
+		</>
+	);
+}
+
+export default UsernameInput;
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import UsernameInput from "../components/login/UsernameInput";
+import PasswordInput from "../components/login/PasswordInput";
+import { GraduationCap } from "lucide-react";
+
+function LoginPage() {
+	return (
+		<div class="absolute top-0 z-[-2] h-screen w-screen bg-[#030d18] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(31,30,87,0.7),rgba(255,255,255,0))]">
+			<div className="w-full gap-5 flex items-center flex-col justify-center h-full">
+				<div className="text-3xl flex flex-col items-center gap-2">
+					<GraduationCap className="size-15 bg-[#00b5dd] p-2 rounded-2xl text-[#030d18] " />
+					<h1 className="italic">
+						PROVIDENCE <span className="text-[#00b5dd]">INSTITUTE</span>
+					</h1>
+					<span className="text-sm tracking-[0.4rem] text-slate-500">PLATAFORMA INSTITUCIONAL</span>
+				</div>
+				<div className="h-100 bg-[##0b1423] w-100 rounded-4xl p-8 border border-slate-800 justify-center shadow-xl shadow-blue-400/10">
+					<div>
+						<h3 className="font-semibold text-xl">Acceso Institucional</h3>
+						<p className="text-sm text-slate-400">Ingresa sus credenciales para acceder a la plataforma</p>
+					</div>
+					<div className="h-70 justify-between flex w-full items-center flex-col">
+						<form action="" className="w-full h-full flex gap-5 justify-center items-center flex-col ">
+							<div className="flex gap-1 flex-col w-full">
+								<span className="text-[#00b5dd] tracking-widest font-semibold text-sm">USUARIO INSTITUCIONAL</span>
+								<UsernameInput />
+							</div>
+							<div className="flex w-full gap-1 flex-col">
+								<span className="text-[#00b5dd] tracking-widest font-semibold text-sm">CONTRASEÑA</span>
+								<PasswordInput />
+							</div>
+						</form>
+						<button className="bg-[#00b5ddb8] cursor-pointer font-semibold  transition-transform hover:translate-y-1 text-[#030d18] w-full h-12 rounded-2xl">
+							Iniciar Sesion
+						</button>
+					</div>
+				</div>
+				<p className="text-center text-slate-600 text-[10px]">
+					@2026 Providence Institute. Todos los derechos reservados. <br /> PPyL Proyecto Universitario{" "}
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default LoginPage;
