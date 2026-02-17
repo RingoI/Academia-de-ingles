c0c47c887fade3a98acca5b7cd1d2264c1561783
RingoI <miltonibarzabal5@gmail.com>
2026-02-17 16:50:08 -0300

    Formulario alumnos

--- frontend/src/components/TablaAlumnos.jsx
+++ frontend/src/components/TablaAlumnos.jsx
@@ -2,41 +2,41 @@ import React, { useEffect } from "react";
 import { usuarioStore } from "../store/usuarios.store";
 
 function TablaAlumnos() {
-	const { alumnos, obtenerAlumnos } = usuarioStore();
+  const { alumnos, obtenerAlumnos } = usuarioStore();
 
-	useEffect(() => {
-		obtenerAlumnos();
-	}, []);
+  useEffect(() => {
+    obtenerAlumnos();
+  }, []);
 
-	console.log("Alumnos:  ", alumnos);
+  console.log("Alumnos:  ", alumnos);
 
-	const cabecera = ["", "Nombre", "Email", "Direccion", "DNI", "Estado"];
+  const cabecera = ["", "Nombre", "Email", "Direccion", "DNI", "Estado"];
 
-	return (
-		<div className="overflow-x-auto">
-			<table className="table rounded-lg overflow-hidden">
-				<thead className="bg-[#0d1526]">
-					<tr>
-						{cabecera.map((c) => (
-							<th>{c}</th>
-						))}
-					</tr>
-				</thead>
-				<tbody className="bg-[#0c1224]">
-					{alumnos.map((a, idx) => (
-						<tr>
-							<th>{idx + 1}</th>
-							<td>{a.nombre}</td>
-							<td>{a.email}</td>
-							<td>{a.direccion}</td>
-							<td>{a.dni}</td>
-							<td>{a.activo ? "Activo" : "Desactivado"}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
+  return (
+    <div className="overflow-x-auto">
+      <table className="table rounded-lg overflow-hidden">
+        <thead className="bg-[#0d1526]">
+          <tr>
+            {cabecera.map((c) => (
+              <th key={c}>{c}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-[#0c1224]">
+          {alumnos.map((a, idx) => (
+            <tr key={a.id}>
+              <th>{idx + 1}</th>
+              <td>{a.nombre}</td>
+              <td>{a.email}</td>
+              <td>{a.direccion}</td>
+              <td>{a.dni}</td>
+              <td>{a.activo ? "Activo" : "Desactivado"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TablaAlumnos;
--- frontend/src/components/TablaDocentes.jsx
+++ frontend/src/components/TablaDocentes.jsx
@@ -2,41 +2,41 @@ import { useEffect } from "react";
 import { usuarioStore } from "../store/usuarios.store";
 
 function TablaDocentes() {
-	const { docentes, obtenerDocentes } = usuarioStore();
+  const { docentes, obtenerDocentes } = usuarioStore();
 
-	useEffect(() => {
-		obtenerDocentes();
-	}, []);
+  useEffect(() => {
+    obtenerDocentes();
+  }, []);
 
-	console.log("Docentes:  ", docentes);
+  console.log("Docentes:  ", docentes);
 
-	const cabecera = ["", "Nombre", "Email", "Direccion", "DNI", "Estado"];
+  const cabecera = ["", "Nombre", "Email", "Direccion", "DNI", "Estado"];
 
-	return (
-		<div className="overflow-x-auto">
-			<table className="table rounded-lg overflow-hidden">
-				<thead className="bg-[#0d1526]">
-					<tr>
-						{cabecera.map((c) => (
-							<th>{c}</th>
-						))}
-					</tr>
-				</thead>
-				<tbody className="bg-[#0c1224]">
-					{docentes.map((d, idx) => (
-						<tr>
-							<th>{idx + 1}</th>
-							<td>{d.nombre}</td>
-							<td>{d.email}</td>
-							<td>{d.direccion}</td>
-							<td>{d.dni}</td>
-							<td>{d.activo ? "Activo" : "Desactivado"}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
+  return (
+    <div className="overflow-x-auto">
+      <table className="table rounded-lg overflow-hidden">
+        <thead className="bg-[#0d1526]">
+          <tr>
+            {cabecera.map((c) => (
+              <th key={c}>{c}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-[#0c1224]">
+          {docentes.map((d, idx) => (
+            <tr key={d.id}>
+              <th>{idx + 1}</th>
+              <td>{d.nombre}</td>
+              <td>{d.email}</td>
+              <td>{d.direccion}</td>
+              <td>{d.dni}</td>
+              <td>{d.activo ? "Activo" : "Desactivado"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TablaDocentes;
